f940145d67933fb6010b91bba3ff27d934ca1f05
Adriano Santos <sleipnir@bsd.com.br>
2021-02-19 17:50:20 -0300

    Format code

--- astreu4j/src/main/java/io/eigr/astreu/Subscriber.java
+++ astreu4j/src/main/java/io/eigr/astreu/Subscriber.java
@@ -8,10 +8,11 @@ import java.time.Duration;
 import java.util.function.Predicate;
 
 public interface Subscriber {
-
-    Publisher<MessageWithContext> bind();
-    Publisher<MessageWithContext> bindWithThrottle(int elements, Duration per, int maximumBurst);
     Subscriber receiveOnly(MessageWithContext.IncomingType... types);
+
     Subscriber filter(Predicate<MessageWithContext> predicate);
 
+    Publisher<MessageWithContext> bind();
+
+    Publisher<MessageWithContext> bindWithThrottle(int elements, Duration per, int maximumBurst);
 }
